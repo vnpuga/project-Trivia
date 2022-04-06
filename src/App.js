f00ec43db22e61afcfd0422d578e3ab3c3ffc4ba
Pedro Tavares <pedrodc1236@gmail.com>
2022-04-06 17:47:09 -0300

    tela de feedback

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
+import Feedback from './pages/FeedBack';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Route exact path="/" component={ Login } />
         <Route exact path="/gameplay" component={ Game } />
         <Route exact path="/settings" component={ Settings } />
+        <Route exact path="/feedback" component={ Feedback } />
       </Switch>
     );
   }
