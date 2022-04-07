503d4ccd2bee1895bb6608bbd4237a02cfeb3b1f
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2022-04-07 17:31:45 -0300

    Adiciona os requisitos 5 e 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -5,16 +5,66 @@ import Header from '../components/Header';
 import { getTrivia } from '../services/apiRequest';
 
 class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      questions: [],
+      answers: [],
+    };
+  }
+
   async componentDidMount() {
     const { token } = this.props;
-    console.log(await getTrivia(token));
+    const questions = await getTrivia(token);
+    this.setState({
+      questions,
+    });
+    this.answerShuffle();
+  }
+
+  answerShuffle = async () => {
+    const { questions: { results } } = this.state;
+    const answersArray = [];
+    const bliu = {
+      answer: results[0].correct_answer,
+      type: 'correct-answer' };
+    answersArray.push(bliu);
+
+    results[0].incorrect_answers.forEach((wrongAnswer) => {
+      answersArray.push({
+        answer: wrongAnswer,
+        type: 'wrong-answer',
+      });
+      const magicNumber = 0.5;
+      answersArray.sort(() => Math.random() - magicNumber);
+    });
+    // https://flaviocopes.com/how-to-shuffle-array-javascript/
+    this.setState({ answers: answersArray });
   }
 
   render() {
     const { placar, name, email } = this.props;
+    const { questions: { results }, answers } = this.state;
     return (
       <div>
         <Header placar={ placar } name={ name } email={ email } />
+        <div>
+          <h1 data-testid="question-category">{ (results) && results[0].category}</h1>
+          <p data-testid="question-text">{ (results) && results[0].question }</p>
+        </div>
+        <div data-testid="answer-options">
+          { answers.map((ans, index) => (
+            <button
+              key={ index }
+              type="button"
+              data-testid={ ans.type }
+            >
+              {ans.answer}
+            </button>
+          ))}
+        </div>
+        <span>Timer</span>
+        <button type="button" style={ { display: 'none' } }>Próxima</button>
       </div>
     );
   }
