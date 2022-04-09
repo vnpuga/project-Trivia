4627698516c0dc99bba6620193f0d75b2bc1d565
Pedro Tavares <pedrodc1236@gmail.com>
2022-04-09 16:51:19 -0300

    css tela de login

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
